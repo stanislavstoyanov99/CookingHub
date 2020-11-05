1fb8fe9e4ac77fad2eb8e6c48cd679db0bc74bbe
Stanislav Stoyanov <slavkata_99@abv.bg>
2020-11-05 12:30:19 +0200

    Update nuget packages and scaffold Identity.

---
+++ src/Web/CookingHub.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Hosting;
+
+[assembly: HostingStartup(typeof(CookingHub.Web.Areas.Identity.IdentityHostingStartup))]
+
+namespace CookingHub.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) =>
+            {
+            });
+        }
+    }
+}
---
+++ src/Web/CookingHub.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -0,0 +1,115 @@
+﻿namespace CookingHub.Web.Areas.Identity.Pages.Account
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Text.Encodings.Web;
+    using System.Threading.Tasks;
+
+    using CookingHub.Common;
+    using CookingHub.Data.Models;
+    using CookingHub.Data.Models.Enumerations;
+    using CookingHub.Web.Areas.Identity.Pages.Account.InputModels;
+
+    using Microsoft.AspNetCore.Authentication;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Identity.UI.Services;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+    using Microsoft.AspNetCore.WebUtilities;
+    using Microsoft.Extensions.Logging;
+
+    [AllowAnonymous]
+#pragma warning disable SA1649 // File name should match first type name
+    public class RegisterModel : PageModel
+#pragma warning restore SA1649 // File name should match first type name
+    {
+        private readonly SignInManager<CookingHubUser> signInManager;
+        private readonly UserManager<CookingHubUser> userManager;
+        private readonly ILogger<RegisterModel> logger;
+        private readonly IEmailSender emailSender;
+
+        public RegisterModel(
+            UserManager<CookingHubUser> userManager,
+            SignInManager<CookingHubUser> signInManager,
+            ILogger<RegisterModel> logger,
+            IEmailSender emailSender)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+            this.logger = logger;
+            this.emailSender = emailSender;
+        }
+
+        [BindProperty(SupportsGet = true)]
+        public RegisterInputModel Input { get; set; }
+
+        public string ReturnUrl { get; set; }
+
+        public IList<AuthenticationScheme> ExternalLogins { get; set; }
+
+        public async Task OnGetAsync(string returnUrl = null)
+        {
+            this.ReturnUrl = returnUrl;
+            this.ExternalLogins = (await this.signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+        }
+
+        public async Task<IActionResult> OnPostAsync(string returnUrl = null)
+        {
+            returnUrl = returnUrl ?? this.Url.Content("~/");
+            this.ExternalLogins = (await this.signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+
+            if (this.ModelState.IsValid)
+            {
+                Enum.TryParse<Gender>(this.Input.SelectedGender, out Gender gender);
+                var user = new CookingHubUser
+                {
+                    UserName = this.Input.Username,
+                    Email = this.Input.Email,
+                    FullName = this.Input.FullName,
+                    Gender = gender,
+                };
+
+                var result = await this.userManager.CreateAsync(user, this.Input.Password);
+
+                if (result.Succeeded)
+                {
+                    this.logger.LogInformation("User created a new account with password.");
+                    await this.userManager.AddToRoleAsync(user, GlobalConstants.UserRoleName);
+
+                    var code = await this.userManager.GenerateEmailConfirmationTokenAsync(user);
+                    code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+
+                    var callbackUrl = this.Url.Page(
+                        "/Account/ConfirmEmail",
+                        pageHandler: null,
+                        values: new { area = "Identity", userId = user.Id, code = code, returnUrl = returnUrl },
+                        protocol: this.Request.Scheme);
+
+                    string htmlMessage = $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.";
+                    await this.emailSender.SendEmailAsync(this.Input.Email, "Confirm your email", htmlMessage: htmlMessage);
+
+                    if (this.userManager.Options.SignIn.RequireConfirmedAccount)
+                    {
+                        return this.RedirectToPage("RegisterConfirmation", new { email = this.Input.Email, returnUrl = returnUrl });
+                    }
+                    else
+                    {
+                        await this.signInManager.SignInAsync(user, isPersistent: false);
+                        return this.LocalRedirect(returnUrl);
+                    }
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    this.ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+
+            // If we got this far, something failed, redisplay form
+            return this.Page();
+        }
+    }
+}
