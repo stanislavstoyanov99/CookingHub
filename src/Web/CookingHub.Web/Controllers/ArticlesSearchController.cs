08e92cafdb40986143ba57c48b38747bf81fee35
Stanislav Stoyanov <slavkata_99@abv.bg>
2020-12-11 23:58:30 +0200

    Add Search results view and implement API controller for ajax searching
    -- Should fix pagination in ajax query
    #4, #5

---
+++ src/Web/CookingHub.Web/Controllers/ArticlesSearchController.cs
@@ -0,0 +1,44 @@
+﻿namespace CookingHub.Web.Controllers
+{
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using CookingHub.Models.ViewModels;
+    using CookingHub.Models.ViewModels.Articles;
+    using CookingHub.Services.Data.Contracts;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ArticlesSearchController : ControllerBase
+    {
+        private const int ArticlesInSearchPage = 4;
+        private readonly IArticlesService articlesService;
+
+        public ArticlesSearchController(IArticlesService articlesService)
+        {
+            this.articlesService = articlesService;
+        }
+
+        [HttpGet("{pageNumber}/{searchTitle}")]
+        public async Task<ActionResult<PaginatedList<ArticleListingViewModel>>> Get(
+            [FromQuery] int? pageNumber,
+            [FromQuery] string searchTitle)
+        {
+            var articles = await Task.Run(() => this.articlesService
+               .GetAllArticlesAsQueryeable<ArticleListingViewModel>()
+               .Where(a => a.Title.ToLower().Contains(searchTitle.ToLower())));
+
+            if (articles.Count() == 0)
+            {
+                return this.NotFound();
+            }
+
+            var articlesPaginated = await PaginatedList<ArticleListingViewModel>
+                .CreateAsync(articles, pageNumber ?? 1, ArticlesInSearchPage);
+
+            return articlesPaginated;
+        }
+    }
+}
