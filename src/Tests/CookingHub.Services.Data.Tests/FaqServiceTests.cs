767c60ec25cf7e1aea426378b047ddeef5045f51
Stanislav Stoyanov <slavkata_99@abv.bg>
2020-12-18 23:45:25 +0200

    Minor changes for RecipeList accordion and initialize tests library.
    -- Add initial state for Faq Service tests
    -- Minor changes to facebook login/register buttons
    -- Fix accordion in RecipeList view
    -- Remove useless code from custom.js
    #5, #14

---
+++ src/Tests/CookingHub.Services.Data.Tests/FaqServiceTests.cs
@@ -0,0 +1,67 @@
+﻿namespace CookingHub.Services.Data.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Reflection;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using CookingHub.Data;
+    using CookingHub.Data.Models;
+    using CookingHub.Data.Repositories;
+    using CookingHub.Services.Data.Contracts;
+    using CookingHub.Services.Mapping;
+
+    using Microsoft.Data.Sqlite;
+    using Microsoft.EntityFrameworkCore;
+
+    public class FaqServiceTests : IAsyncDisposable
+    {
+        private readonly IFaqService faqService;
+        private EfDeletableEntityRepository<FaqEntry> faqEntriesRepository;
+        private SqliteConnection connection;
+        private FaqEntry firstFaqEntry;
+
+        // TODO
+        public FaqServiceTests()
+        {
+            this.InitializeMapper();
+            this.InitializeDatabaseAndRepositories();
+            this.InitializeFields();
+
+            this.faqService = new FaqService(this.faqEntriesRepository);
+        }
+
+        // Write tests here
+
+        public async ValueTask DisposeAsync()
+        {
+            await this.connection.CloseAsync();
+            await this.connection.DisposeAsync();
+        }
+
+        private void InitializeDatabaseAndRepositories()
+        {
+            this.connection = new SqliteConnection("DataSource=:memory:");
+            this.connection.Open();
+            var options = new DbContextOptionsBuilder<CookingHubDbContext>().UseSqlite(this.connection);
+            var dbContext = new CookingHubDbContext(options.Options);
+
+            dbContext.Database.EnsureCreated();
+
+            this.faqEntriesRepository = new EfDeletableEntityRepository<FaqEntry>(dbContext);
+        }
+
+        private void InitializeFields()
+        {
+            this.firstFaqEntry = new FaqEntry
+            {
+                Question = "Test Question?",
+                Answer = "Test Answer.",
+            };
+        }
+
+        private void InitializeMapper() => AutoMapperConfig.
+            RegisterMappings(Assembly.Load("CookingHub.Models.ViewModels"));
+    }
+}
--- src/Web/CookingHub.Web/wwwroot/js/custom.js
+++ src/Web/CookingHub.Web/wwwroot/js/custom.js
@@ -56,25 +56,6 @@
 	});
 	
 	/* ..............................................
-    Special Menu
-    ................................................. */
-	
-	var Container = $('.container');
-	Container.imagesLoaded(function () {
-		var portfolio = $('.special-menu');
-		portfolio.on('click', 'button', function () {
-			$(this).addClass('active').siblings().removeClass('active');
-			var filterValue = $(this).attr('data-filter');
-			$grid.isotope({
-				filter: filterValue
-			});
-		});
-		var $grid = $('.special-list').isotope({
-			itemSelector: '.special-grid'
-		});
-	});
-	
-	/* ..............................................
     BaguetteBox
     ................................................. */
 	
