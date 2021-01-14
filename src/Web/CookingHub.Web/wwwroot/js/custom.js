6bdcec29d525645a33b7f2941fd9dd2771b4f76c
dimitarpashov99 <58173423+dimitarpashov99@users.noreply.github.com>
2021-01-14 20:48:41 +0200

    Minor design issues fixed

--- src/Web/CookingHub.Web/wwwroot/js/custom.js
+++ src/Web/CookingHub.Web/wwwroot/js/custom.js
@@ -56,4 +56,21 @@
 			document.getElementById("logo").style.width = "300px";
 		}
 	}
+	/*..............................................
+	 * AdminNav
+	 ...............................................*/
+	var acc = document.getElementsByClassName("admin-nav");
+	var i;
+
+	for (i = 0; i < acc.length; i++) {
+		acc[i].addEventListener("click", function () {
+			this.classList.toggle("active");
+			var panel = this.nextElementSibling;
+			if (panel.style.display === "block") {
+				panel.style.display = "none";
+			} else {
+				panel.style.display = "block";
+			}
+		});
+	}
 }(jQuery));
