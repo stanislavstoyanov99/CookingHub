6694bab15e2e1e7f8a17aae3c7cfe3d5890c8b5a
Stanislav Stoyanov <slavkata_99@abv.bg>
2021-01-14 21:23:31 +0200

    Add minor refactor in Index.cshtml and ManageNav.cshtml
    #13, #5
    -- Refactor and create IndexInputModel

---
+++ src/Web/CookingHub.Web/Areas/Identity/Pages/Account/InputModels/IndexInputModel.cs
@@ -0,0 +1,11 @@
+﻿namespace CookingHub.Web.Areas.Identity.Pages.Account.InputModels
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class IndexInputModel
+    {
+        [Phone]
+        [Display(Name = "Phone number")]
+        public string PhoneNumber { get; set; }
+    }
+}
